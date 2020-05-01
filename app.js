8c0449987c717bb5f5c86ce01d1eb2d7636b0de7
Ángel Sánchez <hangelsanchez2@hotmail.com>
2020-05-01 16:58:52 +0000

    Se acaba el ejercicio

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
-// we declare a new global variable containing an array that represents the ballons map
-var allBalloons = ['green', 'purple', 'grey', 'pink', 'brown', 'red', 'green', 'grey', 'pink', 'blue', 'red', 'brown', 'purple', 'green', 'red', 'grey', 'pink', 'purple', 'orange', 'red']
-var ballonsMap = ['green'];
-var activeBalloons = 20;
+let ballonsMap = []; //Declaramos e inicializamos un Array vacio para luego darle valor
+
+const GetRandomPosition = () => {
+    for (i = 0; i < 20; i++) {
+        ballonsMap[i] = '#' + Math.floor(Math.random() * 16777215).toString(16); //recorro el arreglo BallonsMap y lo voy rellenando en la posición i, durante 20 veces.
+    }
+};
+
+GetRandomPosition();//Hasta que no invoquemos a la función flecha GetRandomPOsition no se ejecuta
+
+let activeBalloons = ballonsMap.length; //Valor inicial del contador de globos es igual a la longitud del array, 20 (la longitud la marcamos en el bucle for)
 
 // poping a balloon is basically turning his color to null (no color)
 const popBalloon = (position) => {
-    if (color == null) {
-    } render();
+    console.log("Hello World");
+    // set the color to null on the balloon position
+    ballonsMap[position] = null;
+    activeBalloons -= 1;//Cuando se explota un globo se va restando en 1 unidad de la variable activeBallons
+    render(); //Invocamos a la función render, la cual ejecuta el código de la función flecha de más abajo.
 }
 
 const render = () => {
-    
+
     // convert ballons map of colors into real html balloons
     const ballons = ballonsMap.map((color, position) => {
-        return `<div class="balloon active" id=ballon-1></div><div class="balloon active" id=ballon-2></div><div class="balloon active" id=ballon-3></div><div class="balloon active" id=ballon-4></div><div class="balloon active" id=ballon-5></div><div class="balloon active" id=ballon-6></div><div class="balloon active" id=ballon-7></div><div class="balloon active" id=ballon-8></div><div class="balloon active" id=ballon-9></div><div class="balloon active" id=ballon-10></div><div class="balloon active" id=ballon-11></div><div class="balloon active" id=ballon-12></div><div class="balloon active" id=ballon-13></div><div class="balloon active" id=ballon-14></div><div class="balloon active" id=ballon-15></div><div class="balloon active" id=ballon-16></div><div class="balloon active" id=ballon-17></div><div class="balloon active" id=ballon-18></div><div class="balloon active" id=ballon-19></div><div class="balloon active" id=ballon-20></div>`; // <--- render each balloon
+        if (color == null) {
+            return `<div class="balloon popped" id="${position}" onClick="popBalloon(${position})"></div>`; //con $ inyectamos a la variable position del arreglo ballonsMap la posición
+        }
+
+        else {
+            return `<div class="balloon active" style="background: ${color};" id="${position}" onClick="popBalloon(${position})"></div>`;
+        }
+        // <--- render each balloon
     });
 
     document.querySelector("#balloon-count").innerHTML = activeBalloons; // <-- rende the balloon count into the DOM
     document.querySelector("#balloon-map").innerHTML = ballons.join(''); // <-- render the balloons into the DOM
 
-    if(activeBalloons == 0) window.location.reload(); // <--- reload website when no more balloons are left
+    if (activeBalloons == 0) window.location.reload(); // <--- reload website when no more balloons are left
 }
 
 // this makes the "render" function trigger when the website starts existing
